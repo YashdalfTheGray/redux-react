08b3a7549bfebfcc4a359eda80dc3c1b8ef3ce63
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-10 19:37:46 -0400

    added support for a counter list

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import { createStore } from 'redux';
 import AppBar from 'material-ui/lib/app-bar';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 import Counter from './components/counter';
 import counterReducer from './reducers/counterReducer';
 
 class App extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counters: counterStore.getState()
+        };
+
+        this.unsubscribe = counterStore.subscribe(() => {
+            this.setState({
+                counters: counterStore.getState()
+            });
+        });
+    }
+
+    componentDidMount() {
+        counterStore.dispatch({ type: '' });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    addCounter() {
+        counterStore.dispatch({ type: 'ADD_COUNTER' });
+    }
+
     render() {
+        var counters = this.state.counters.map((c, i) => {
+            return <Counter key={i} index={i} />
+        });
+
         var counterListStyle = {
             marginTop: '16px'
         }
@@ -16,8 +47,13 @@ class App extends React.Component {
                 <AppBar
                     title="Redux" />
                 <div style={counterListStyle}>
-                    <Counter />
+                    {counters}
                 </div>
+                <RaisedButton
+                    style={{ margin: '0px 16px', marginTop: '24px' }}
+                    label="Add Counter"
+                    primary={true}
+                    onTouchTap={this.addCounter} />
             </div>
         );
     }
