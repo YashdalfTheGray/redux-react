8899a763a6612e8a4b41981feb4e38c553bef386
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-10 18:48:23 -0400

    added counter component, now exporting a counterstore

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { createStore } from 'redux';
 import AppBar from 'material-ui/lib/app-bar';
 
+import Counter from './components/counter';
+import counterReducer from './stores/counterReducer';
+
 class App extends React.Component {
-    navMenuClick() {
-        console.log('Nav menu button clicked!');
-    }
 
     render() {
+        var counterListStyle = {
+            marginTop: '16px'
+        }
         return (
-            <AppBar
-                title="React Starter"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-                onLeftIconButtonTouchTap={this.navMenuClick} />
+            <div>
+                <AppBar
+                    title="Redux" />
+                <div style={counterListStyle}>
+                    <Counter />
+                </div>
+            </div>
         );
     }
 }
 
 export default App;
+
+const counterStore = createStore(counterReducer);
+export { counterStore };
