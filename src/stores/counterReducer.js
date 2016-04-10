e3830e08516dbcd6dd1df23f8db9b582f52ba6a4
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-10 18:47:37 -0400

    added a reducer function for a counter store

---
+++ src/stores/counterReducer.js
@@ -0,0 +1,17 @@
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+            break;
+        case 'DECREMENT':
+            return state - 1;
+            break;
+        case 'RESET':
+            return 0;
+            break;
+        default:
+            return state;
+    }
+};
+
+export default counterReducer;
