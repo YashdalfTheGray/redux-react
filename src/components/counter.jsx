1f5d7f6a169b85a080d3ec7ff2ba6d227db3f04e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-10 19:37:33 -0400

    added a remove counter action to the counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,26 +12,39 @@ export default class Counter extends React.Component {
         super(props);
 
         this.state = {
-            value: counterStore.getState()
+            value: counterStore.getState()[this.props.index]
         };
 
-        counterStore.subscribe(() => {
+        this.unsubscribe = counterStore.subscribe(() => {
             this.setState({
-                value: counterStore.getState()
+                value: counterStore.getState()[this.props.index]
             });
         });
+
+        this.incrementCounter = this.incrementCounter.bind(this);
+        this.decrementCounter = this.decrementCounter.bind(this);
+        this.resetCounter = this.resetCounter.bind(this);
+        this.removeCounter = this.removeCounter.bind(this);
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
     }
 
     incrementCounter() {
-        counterStore.dispatch({ type: 'INCREMENT' });
+        counterStore.dispatch({ type: 'INCREMENT', index: this.props.index });
     }
 
     decrementCounter() {
-        counterStore.dispatch({ type: 'DECREMENT' });
+        counterStore.dispatch({ type: 'DECREMENT', index: this.props.index });
     }
 
     resetCounter() {
-        counterStore.dispatch({ type: 'RESET' });
+        counterStore.dispatch({ type: 'RESET', index: this.props.index });
+    }
+
+    removeCounter() {
+        counterStore.dispatch({ type: 'REMOVE_COUNTER', index: this.props.index });
     }
 
     render() {
@@ -52,6 +65,9 @@ export default class Counter extends React.Component {
                     <FlatButton
                         label="Reset"
                         onTouchTap={this.resetCounter} />
+                    <FlatButton
+                        label="Remove Counter"
+                        onTouchTap={this.removeCounter} />
                 </CardActions>
             </Card>
         );
