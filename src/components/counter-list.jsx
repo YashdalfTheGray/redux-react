0b5f5023c0714270e469f24a5d23f79fd654c853
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-11 22:06:48 -0400

    pulled out the counter list to another component

---
+++ src/components/counter-list.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+import Counter from './counter';
+import { counterStore } from '../app';
+
+export default class CounterList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counters: counterStore.getState()
+        };
+
+        this.unsubscribe = counterStore.subscribe(() => {
+            this.setState({
+                counters: counterStore.getState()
+            });
+        });
+    }
+
+    componentDidMount() {
+        counterStore.dispatch({ type: '' });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    addCounter() {
+        counterStore.dispatch({ type: 'ADD_COUNTER' });
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={{ marginTop: '16px' }}>
+                    {this.state.counters.map((c, i) => {
+                        return <Counter key={i} index={i} />
+                    })}
+                </div>
+                <RaisedButton
+                    style={{ margin: '0px 16px', marginTop: '24px' }}
+                    label="Add Counter"
+                    secondary={true}
+                    onTouchTap={this.addCounter} />
+            </div>
+        );
+    }
+}
