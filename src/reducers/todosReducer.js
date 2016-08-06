15f6aee37cf7d4e6d98de2a7af6ea1ab6a218e7f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-08-05 21:48:55 -0400

    pulled the todo reducer out of the todoslist reducer

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,13 +1,34 @@
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+            break;
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+            else {
+                return {
+                    ...state,
+                    completed: !state.completed
+                };
+            }
+            break;
+        default:
+            return state;
+    }
+}
+
 const todosReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
+                todo(undefined, action)
             ];
             break;
         case 'REMOVE_COMPLETED_TODOS':
@@ -18,17 +39,7 @@ const todosReducer = (state = [], action) => {
             });
             break;
         case 'TOGGLE_TODO':
-            return state.map(todo => {
-                if (todo.id !== action.id) {
-                    return todo;
-                }
-                else {
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    };
-                }
-            });
+            return state.map(t => todo(t, action));
             break;
         default:
             return state;
